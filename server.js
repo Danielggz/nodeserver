4e0bd6f6d9250662c74914e1c1fc91b3c38be09f
Danielggz <dggz1993@gmail.com>
2018-05-22 13:05:27 +0200

    entrar

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
-var express = require('express');
+var express = require('express'); //LIBRERÍA EXPRESS
 var bodyParser = require('body-parser')
 var fs = require('fs');
-var app = express();
+var mysql = require('mysql'); //LIBRERÍA MYSQL
+var app = express(); 
+
+//<------CONECTAR CON BASE DE DATOS MYSQL--------> 
+
+// NPM INSTALL MYSQL --SAVE (GUARDAR MYSQL EN PACKAGE.JSON)
+
+var connection = mysql.createConnection({
+   host: 'localhost',
+   user: 'master',
+   password: 'abc123.',
+   database: 'tareasdb',
+   port: 3306 //Puerto por defecto
+});
+connection.connect(function(error){
+   if(error){
+      throw error;
+   }else{
+      console.log('Conexion correcta. Conectado como ' + connection.user);
+   }
+});
+
 
 // create application/json parser
 var jsonParser = bodyParser.json();
@@ -15,6 +36,7 @@ var listaTareas = [];
 //INICIO EXPRESS
 app.use(express.static('www/tareas'));
 
+
 //MÉTODO PARA COMPROBAR SI EXISTE EL FICHERO Y CARGARLO
 fileExists("./www/tareas/BD/almacen.json", function(err, exists){
   if(err) {
@@ -27,10 +49,23 @@ fileExists("./www/tareas/BD/almacen.json", function(err, exists){
 });
 
 //EJECUTA LA LISTA DE TAREAS EN EL INDEX
+/*
 app.get('/', function(req, res){
   
   fs.readFile('./www/tareas/indextareas.html', 'utf-8', function(err, text){
     text = text.replace("[sustituir]", cargarTareas(listaTareas) );
+
+    res.send(text);
+  });
+  
+  
+});
+*/
+
+app.get('/', function(req, res){
+  
+  fs.readFile('./www/tareas/indextareas.html', 'utf-8', function(err, text){
+    text = text.replace("[sustituir]", cargarTareasBD() );
 
     res.send(text);
   });
@@ -43,7 +78,7 @@ app.get('/eliminar/:id?', function(req, res){
 
 
     listaTareas.splice(req.query.id,1);
-    actualizar();
+    actualizarFich();
     console.log('tarea eliminada');
     res.redirect('/');
   
@@ -97,7 +132,7 @@ app.post('/editar', function(req, res){
 
   
 
-  actualizar();
+  actualizarFich();
   
   console.log('nueva tarea guardada');
 
@@ -109,6 +144,11 @@ app.post('/editar', function(req, res){
 app.post('/', function(req, res) {
 
   console.log('Peticion recibida');
+  console.log("");
+  console.log("");
+  console.log("<----------------------NUEVOS DATOS RECIBIDOS------------------------->");
+  console.log("");
+  console.log("");
 
   var nombre = req.body.nombre || '';
   var tarea = req.body.tarea || '';
@@ -116,7 +156,6 @@ app.post('/', function(req, res) {
   var nomb = nombre;
   var tar = tarea;
   
-  console.log(listaTareas);
 
   listaTareas.push({nombre:nomb, tarea:tar});
   
@@ -124,22 +163,38 @@ app.post('/', function(req, res) {
   console.log('nuevo dato: ' + tarea);
   //console.log(arraytareas);
 
-  console.log('Guardando datos..');
+  console.log("");
+  console.log("");
+  console.log("<--------------------GUARDAR DATOS EN FICHERO----------------------->");
+  console.log("");
+  console.log("");
+  console.log('Guardando datos en fichero..');
 
-  actualizar();
+  actualizarFich();
 
-  console.log('nueva tarea guardada');
+  console.log('nueva tarea guardada en fichero');
+
+  //INTRODUCIR DATOS EN MYSQL TAREASDB
+  console.log("");
+  console.log("");
+  console.log("<-------------------------BASE DE DATOS---------------------------->");
+  console.log("DATOS ACTUALIZADOS");
+  console.log("");
+  console.log("");
+  console.log('Actualizando base de datos TAREASDB..');
+
+  actualizarBD(nombre, tarea);
   
   res.redirect('/');
   
 });
 
 
-//<-----------MÉTODOS-------------->>
+//<-----------MÉTODOS-------------->
 
 
 //CARGAR LA LISTA DE TAREAS EN EL INDEX
-function cargarTareas(tareas) {
+function cargarTareas() {
 
   var lista = "";
   for (var indice in tareas) {
@@ -158,8 +213,42 @@ function cargarTareas(tareas) {
   return lista;
 }
 
-//ACTUALIZAR FICHERO DE DATOS
-function actualizar()
+function cargarTareasBD() //CARGAR LA LISTA DE TAREAS PERO UTILIZANDO LA BASE DE DATOS CON SELECT
+{
+  var arrayDB = [];
+  var selectDB = connection.query("SELECT * FROM tareas", function(err, result){
+    if(err)
+    {
+      throw err;
+    }
+    else
+    {
+      console.log(result);
+      arrayDB = result;
+    }
+  });
+
+
+
+  var lista = "";
+  for (var indice in arrayDB) {
+    
+    var fila = "<tr> <td>[id]</td> <td>[nombre]</td> <td>[tarea]</td> <td><a href='/eliminar?id= " + indice + "' class='button'> Eliminar tarea </a> </button></td> <td><a href='/editar?id=" + indice + "'> Editar tarea </a> </td></tr>";
+
+      fila = fila.replace("[id]", indice);
+      //fila = fila.split("[id]").join(indice);
+      fila = fila.replace("[nombre]", arrayDB[indice].nombre);
+      fila = fila.replace("[tarea]", arrayDB[indice].tarea);
+
+      lista += fila;
+  }
+
+  
+  return lista;
+}
+
+//actualizar FICHERO DE DATOS
+function actualizarFich()
 {
   fs.writeFile('./www/tareas/BD/almacen.json', JSON.stringify(listaTareas), function(err){
     if (err) 
@@ -168,6 +257,31 @@ function actualizar()
     }
   });
 }
+
+//actualizar BASE DE DATOS
+function actualizarBD(newnom, newtar)
+{
+  
+  var nombre = newnom;
+  var tarea = newtar;
+
+  console.log("nombre: " + nombre);
+  console.log("tarea: " + tarea);
+  var insertBD = connection.query("INSERT INTO tareas VALUES(null, '"+ nombre +"','"+ tarea +"')");
+
+  console.log("");
+  console.log("NUEVO DATO EN BD: ");
+
+  var selectDB = connection.query("SELECT nombre, tarea from tareas where nombre='" + nombre + "' and tarea='" + tarea + "'", function(err, result)
+  {
+    if (err) throw err;
+    console.log(result);
+  });
+
+
+}
+
+//<------------------------------->
 
 //COMPROBAR SI FICHERO EXISTE
 function fileExists(file, cb) {
@@ -193,3 +307,6 @@ function fileExists(file, cb) {
 var server = app.listen(3000, function () {
   console.log('Servidor web iniciado');
 });
+
+//FIN CONEXIÓN CON MYSQL
+//connection.end();
